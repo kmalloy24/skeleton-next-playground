0bc2941c31c3f0c8ea1b6fc7d6c2a6ac76e62071
Kyle Malloy <kmalloy24@gmail.com>
2024-07-26 16:06:13 -0400

    FeaturedGallery

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import GridGallery from '$lib/components/GridGallery.svelte';
 	import QuadGallery from '$lib/components/QuadGallery.svelte';
 	import MasonryGallery from '$lib/components/MasonryGallery.svelte';
+	import FeaturedGallery from '$lib/components/FeaturedGallery.svelte';
 
 	import { Tabs } from '@skeletonlabs/skeleton-svelte';
 
@@ -20,7 +21,11 @@
 <div class="m-8">
 	<div class="my-4 flex items-center justify-between">
 		<h1 class="h1 text-primary-500">Skeleton NEXT Image Layouts</h1>
-		<a href="##" class="btn preset-filled-secondary-500">
+		<a
+			href="https://github.com/kmalloy24/skeleton-next-playground"
+			target="_blank"
+			class="btn preset-filled-secondary-500"
+		>
 			<span>Code</span>
 			<span>&rarr;</span>
 		</a>
@@ -31,6 +36,7 @@
 			<Tabs.Control bind:group name="grid" on:click={handleTabChange}>Grid</Tabs.Control>
 			<Tabs.Control bind:group name="quad" on:click={handleTabChange}>Quad</Tabs.Control>
 			<Tabs.Control bind:group name="masonry" on:click={handleTabChange}>Masonry</Tabs.Control>
+			<Tabs.Control bind:group name="feat" on:click={handleTabChange}>Featured</Tabs.Control>
 		{/snippet}
 		{#snippet panels()}
 			{#if loading}
@@ -40,21 +46,20 @@
 					></figure>
 				</div>
 			{:else}
-				<Tabs.Panel bind:group value="grid">
-					<div class="m-12">
+				<div class="m-12">
+					<Tabs.Panel bind:group value="grid">
 						<GridGallery />
-					</div>
-				</Tabs.Panel>
-				<Tabs.Panel bind:group value="quad">
-					<div class="m-12">
+					</Tabs.Panel>
+					<Tabs.Panel bind:group value="quad">
 						<QuadGallery />
-					</div>
-				</Tabs.Panel>
-				<Tabs.Panel bind:group value="masonry">
-					<div class="m-12">
+					</Tabs.Panel>
+					<Tabs.Panel bind:group value="masonry">
 						<MasonryGallery />
-					</div>
-				</Tabs.Panel>
+					</Tabs.Panel>
+					<Tabs.Panel bind:group value="feat">
+						<FeaturedGallery />
+					</Tabs.Panel>
+				</div>
 			{/if}
 		{/snippet}
 	</Tabs>
